1eada099e9d245bdcf7bc6819b23c65802765654
Zhou Zhou <zhouz2@rose-hulman.edu>
2017-12-17 12:50:02 -0500

    Add configurations for protobuf definitions

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 
 #include <boost/filesystem.hpp>
+#include "proto/message.pb.h"
 
 int main() {
-
+  message::Message m;
   std::vector<std::string> temp = {"test1", "test2"};
 
   std::cout << "Hello" << std::endl;
