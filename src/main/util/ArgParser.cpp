1c36dd3007b76112f53030e31757738e95189b8b
Josh Heidecker <josheidecker@gmail.com>
2019-01-10 17:33:07 -0500

    Option for using hard LUT only shows for RISCV.
    Also changed --use-lut arg to --use-hard-lut to clarify that opiton
    uses a hardware LUT.
    
    Closes #38

--- src/main/util/ArgParser.cpp
+++ src/main/util/ArgParser.cpp
@@ -23,7 +23,9 @@ bool argparser::parse(int argc, char **argv, po::variables_map &vm) {
     ("output-format", po::value<string>()->default_value("CSV"), "output file format (CSV, default CSV)")
     ("output-precision,p", po::value<int>()->default_value(7), "number of decimal points to be displayed/written to output")
 #if INCLUDE_LUT_SUPPORT
-    ("use-lut,l", "whether to use LUT during computation")
+#if RISCV
+    ("use-hard-lut,l", "whether to use hardware LUT during computation")
+#endif
     ("use-soft-lut,s", "whether to use software LUT during computation")
 #endif
     ("verbose-level,v", po::value<int>()->default_value(1), "set verbose level printed to output stream (1 - 3, default 1)")
