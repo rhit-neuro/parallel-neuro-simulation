5e12974b14b9b3cf7658f90b4d1d051bb3eed31d
Cooper Wayne Winans <winanscw@rose-hulman.edu>
2019-01-16 14:48:09 -0500

    switch offset and slope funct code

--- src/main/rocc/lut_support.h
+++ src/main/rocc/lut_support.h
@@ -5,15 +5,15 @@
 
 #include "../../include/rocc-software/src/xcustom.h"
 
-#define FUNCT_LOOKUPSLOPE 0 //opcode 0b0001011
-#define FUNCT_LOOKUPOFFSET 1 //opcode 0b0101011
+#define FUNCT_LOOKUPOFFSET 0
+#define FUNCT_LOOKUPSLOPE 1
 
 #define XCUSTOM_ACC 0
 
-#define lookupSlope(rocc_rd, rocc_rs1, rocc_rs2) \
-  ROCC_INSTRUCTION(0, rocc_rd, rocc_rs1, rocc_rs2, FUNCT_LOOKUPSLOPE)
-
 #define lookupOffset(rocc_rd, rocc_rs1, rocc_rs2) \
   ROCC_INSTRUCTION(0, rocc_rd, rocc_rs1, rocc_rs2, FUNCT_LOOKUPOFFSET)
+
+#define lookupSlope(rocc_rd, rocc_rs1, rocc_rs2) \
+  ROCC_INSTRUCTION(0, rocc_rd, rocc_rs1, rocc_rs2, FUNCT_LOOKUPSLOPE)
 
 #endif  // LUT_SUPPORT_H
