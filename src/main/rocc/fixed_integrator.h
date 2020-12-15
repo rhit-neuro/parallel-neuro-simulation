32241d80d36bd51a6741c85692a9c8a74b4a35ed
Andrew Johnson <johnsoa8@rose-hulman.edu>
2020-12-15 10:32:05 -0500

    header file for connecting with rocc accelerator

---
+++ src/main/rocc/fixed_integrator.h
@@ -0,0 +1,17 @@
+// See LICENSE for license details.
+#if RISCV
+
+#include "../../include/rocc-software/src/xcustom.h"
+
+#define FUNCT_WHOLE_STEP 0
+#define FUNCT_HALF_STEP 1
+
+#define XCUSTOM_ACC 0
+
+#define integrateWholeStep(rocc_rd, rocc_rs1, rocc_rs2) \
+	ROCC_INSTRUCTION(0, rocc_rd, rocc_rs1, rocc_rs2, FUNCT_WHOLE_STEP)
+	
+#define integrateHalfStep(rocc_rd, rocc_rs1, rocc_rs2) \
+	ROCC_INSTRUCTION(0, rocc_rd, rocc_rs1, rocc_rs2, FUNCT_HALF_STEP)
+
+#endif //RISCV
